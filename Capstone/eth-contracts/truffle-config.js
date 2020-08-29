0a186f96924428eeafdded279578b69dc4a9bdf1
Sreesankar G Warrier <sreesankargwarrier@gmail.com>
2020-08-29 18:10:13 +0530

    Update truffle-config.js

--- Capstone/eth-contracts/truffle-config.js
+++ Capstone/eth-contracts/truffle-config.js
@@ -24,7 +24,7 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "quarter exhibit bench relax rotate three duty denial hurdle slight interest pumpkin";
+var mnemonic = "YOUR MNEMONIC";
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -50,7 +50,7 @@ module.exports = {
      },
      rinkeby: {
       provider: function() { 
-        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/1496615348ad40819bbc0f767c78c23d') 
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/YOUR TOKEN') 
       },
       network_id: 4,
     }
